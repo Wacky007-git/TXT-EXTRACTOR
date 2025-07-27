48570f874de480acf567c69b3bb0328b0b9926a4
Wacky007-git <78213969+Wacky007-git@users.noreply.github.com>
2025-07-27 14:55:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo..."
-git clone https://github.com/Popeye68/TXT-EXTRACTOR
+git clone https://github.com/Wacky007-git/TXT-EXTRACTOR
 cd /TXT-EXTRACTOR
 pip install -r requirements.txt
 echo "Starting Bot..."
